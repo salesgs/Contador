49fdb337b81f59c4af35d46d7e8efcadda92755f
gabriel sales <gabrielhub16@gmail.com>
2024-01-24 19:40:31 -0300

    versão 5

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@ import { decrement } from "./decrement.js" ;
 import {resetNumber} from "./reset.js";
 import { increment } from "./imcrement.js";
 
-// DECREMENTAR
+
 const btnDecrement = document.querySelector(".decrement");
 btnDecrement.addEventListener("click",decrement);
 
-// RESETAR
 const btnReset = document.querySelector(".reset");
 btnReset.addEventListener("click",resetNumber);
-// INCREMENTAR
+
 const bntIncrement = document.querySelector(".increment");
 bntIncrement.addEventListener("click",increment);
